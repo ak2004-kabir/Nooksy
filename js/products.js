7343ef0fea4b20eec1a2fa2dcff1cb965ad0058d
ak2004-kabir <abdulkabir0225@gmail.com>
2026-06-25 00:12:45 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -11,6 +11,13 @@ const products = [
     originalPrice: 1299,
     badge: 'sale',
     images: [
+      'https://images.nooksy.in/tb1/tb1-1.jpg',
+      'https://images.nooksy.in/tb1/tb1-2.jpg',
+      'https://images.nooksy.in/tb1/tb1-3.jpg',
+      'https://images.nooksy.in/tb1/tb1-4.jpg',
+      'https://images.nooksy.in/tb1/tb1-5.jpg',
+      'https://images.nooksy.in/tb1/tb1-6.jpg',
+      'https://images.nooksy.in/tb1/tb1-7.jpg'
       ],
     colors: [],
     sizes: [],
