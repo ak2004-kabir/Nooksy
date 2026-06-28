8fd4454d6407f30936bb02e6e8c3aacad8ce26a0
ak2004-kabir <abdulkabir0225@gmail.com>
2026-06-28 22:19:11 +0530

    Update products.js

--- js/products.js
+++ js/products.js
@@ -66,8 +66,7 @@ const products = [
       ],
     colors: [],
     sizes: [],
-    description: 'This tote bag can be used for several purposes, such as grocery shopping, carrying books, laptop bags, gym bags, or even as a fashion accessory. Our tote bags are a more sustainable alternative to plastic shopping bags.,
-    details: 'Dimensions: 20oz. 600ml',
+    description: 'This tote bag can be used for several purposes.',
     inStock: true
   },
   {
